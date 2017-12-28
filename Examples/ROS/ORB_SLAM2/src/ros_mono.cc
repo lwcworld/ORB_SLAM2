125369886a89f307ab58472506abba5047fc1fa1
pasrom <32713472+pasrom@users.noreply.github.com>
2017-12-28 18:52:54 +0100

    added pullrequests #102 and #443 into ros_mono.cc
    Publish camera pose as tf ros message #102.
    Added publishing of pointcloud and position to ros rgbd example #443.

--- Examples/ROS/ORB_SLAM2/src/ros_mono.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_mono.cc
@@ -40,6 +40,14 @@
 
 #include "std_msgs/Float64.h"
 
+#include"../../../include/MapPoint.h"
+
+#include "sensor_msgs/PointCloud.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/Point32.h"
+#include "tf/transform_datatypes.h"
+#include <tf/transform_broadcaster.h>
+
 // Converts degrees to radians.
 #define degreesToRadians(angleDegrees) (angleDegrees * M_PI / 180.0)
 
@@ -48,6 +56,7 @@
 
 
 using namespace std;
+using namespace tf;
 void imageCallback(const sensor_msgs::CompressedImageConstPtr& msg);
 class ImageGrabber
 {
@@ -55,12 +64,15 @@ public:
     ImageGrabber(ORB_SLAM2::System* pSLAM):mpSLAM(pSLAM){}
 
     void GrabImage(const sensor_msgs::ImageConstPtr& msg);
-	
+    //void GrabImage(const sensor_msgs::ImageConstPtr& msg, ros::Publisher pos_pub, ros::Publisher cloud_pub);
 
 
     ORB_SLAM2::System* mpSLAM;
 
     double roll, yaw,pitch;
+
+    ros::Publisher cloud_pub;
+    ros::Publisher pos_pub;
 };
 
 int main(int argc, char **argv)
@@ -79,19 +91,22 @@ int main(int argc, char **argv)
     ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
 
     ImageGrabber igb(&SLAM);
-
+    
     ros::NodeHandle nodeHandler;
-	//ros::Subscriber sub = nodeHandler.subscribe("/camera/image_raw/compressed", 1, &imageCallback);
+    //ros::Subscriber sub = nodeHandler.subscribe("/camera/image_raw/compressed", 1, &imageCallback);
     ros::Publisher robotYaw = nodeHandler.advertise<std_msgs::Float64>("/robot/yaw", 100);
-    ros::Subscriber sub = nodeHandler.subscribe("/camera/camera_eigen", 1, &ImageGrabber::GrabImage,&igb);
+    igb.cloud_pub = nodeHandler.advertise<sensor_msgs::PointCloud>("/slam/pointcloud", 1);
+    igb.pos_pub = nodeHandler.advertise<geometry_msgs::PoseStamped>("/slam/pos", 1);
+    
+    ros::Subscriber sub = nodeHandler.subscribe("/camera/camera_eigen", 1,&ImageGrabber::GrabImage,&igb);
     ros::Rate loop_rate(50);
     //ros::spin();
     while (ros::ok())
-	{
+    {
         robotYaw.publish(igb.yaw);
-		ros::spinOnce();
+        ros::spinOnce();
         loop_rate.sleep();
-	}
+    }
 
     // Stop all threads
     SLAM.Shutdown();
@@ -104,14 +119,15 @@ int main(int argc, char **argv)
     return 0;
 }
 
+//void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg, ros::Publisher pos_pub, ros::Publisher cloud_pub)
 void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
 {
     // Copy the ros image message to cv::Mat.
     cv_bridge::CvImageConstPtr cv_ptr;
-	cv::Mat image;
+    cv::Mat image;
     try
     {
-		//cv_ptr = cv_bridge::toCvShare(cv::imdecode(cv::Mat(msg->data),1));//convert compressed image data to cv::Mat
+        //cv_ptr = cv_bridge::toCvShare(cv::imdecode(cv::Mat(msg->data),1));//convert compressed image data to cv::Mat
         cv_ptr = cv_bridge::toCvShare(msg);
     }
     catch (cv_bridge::Exception& e)
@@ -121,67 +137,100 @@ void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
     }
 
     cv::Mat pose = mpSLAM->TrackMonocular(cv_ptr->image,cv_ptr->header.stamp.toSec());
-	//cv::Mat pose = mpSLAM->TrackMonocular(image,cv_ptr->header.stamp.toSec());
+    //cv::Mat pose = mpSLAM->TrackMonocular(image,cv_ptr->header.stamp.toSec());
 
-	if (pose.empty()){
+    if (pose.empty()){
         return;
-	}
+    }
 
 
-    /* global left handed coordinate system */
-	static cv::Mat pose_prev = cv::Mat::eye(4,4, CV_32F);
-	static cv::Mat world_lh = cv::Mat::eye(4,4, CV_32F);
-	// matrix to flip signs of sinus in rotation matrix, not sure why we need to do that
-	static const cv::Mat flipSign = (cv::Mat_<float>(4,4) <<   1,-1,-1, 1,
-			-1, 1,-1, 1,
-			-1,-1, 1, 1,
-			1, 1, 1, 1);
+     /* global left handed coordinate system */
+    static cv::Mat pose_prev = cv::Mat::eye(4,4, CV_32F);
+    static cv::Mat world_lh = cv::Mat::eye(4,4, CV_32F);
+    // matrix to flip signs of sinus in rotation matrix, not sure why we need to do that
+    static const cv::Mat flipSign = (cv::Mat_<float>(4,4) <<   1,-1,-1, 1,
+                                                               -1, 1,-1, 1,
+                                                               -1,-1, 1, 1,
+                                                                1, 1, 1, 1);
 
-	//prev_pose * T = pose
-	cv::Mat translation =  (pose * pose_prev.inv()).mul(flipSign);
-	world_lh = world_lh * translation;
-	pose_prev = pose.clone();
+    //prev_pose * T = pose
+    cv::Mat translation =  (pose * pose_prev.inv()).mul(flipSign);
+    world_lh = world_lh * translation;
+    pose_prev = pose.clone();
 
-	tf::Matrix3x3 tf3d;
-	tf3d.setValue(pose.at<float>(0,0), pose.at<float>(0,1), pose.at<float>(0,2),
-			pose.at<float>(1,0), pose.at<float>(1,1), pose.at<float>(1,2),
-			pose.at<float>(2,0), pose.at<float>(2,1), pose.at<float>(2,2));
 
-	tf::Vector3 cameraTranslation_rh( world_lh.at<float>(0,3),world_lh.at<float>(1,3), - world_lh.at<float>(2,3) );
+    /* transform into global right handed coordinate system, publish in ROS*/
+    tf::Matrix3x3 cameraRotation_rh(  - world_lh.at<float>(0,0),   world_lh.at<float>(0,1),   world_lh.at<float>(0,2),
+                                  - world_lh.at<float>(1,0),   world_lh.at<float>(1,1),   world_lh.at<float>(1,2),
+                                    world_lh.at<float>(2,0), - world_lh.at<float>(2,1), - world_lh.at<float>(2,2));
 
-	//rotate 270deg about x and 270deg about x to get ENU: x forward, y left, z up
-	const tf::Matrix3x3 rotation270degXZ(   0, 1, 0,
-			0, 0, 1,
-			1, 0, 0);
+    tf::Vector3 cameraTranslation_rh( world_lh.at<float>(0,3),world_lh.at<float>(1,3), - world_lh.at<float>(2,3) );
 
-	static tf::TransformBroadcaster br;
+    //rotate 270deg about x and 270deg about x to get ENU: x forward, y left, z up
+    const tf::Matrix3x3 rotation270degXZ(   0, 1, 0,
+                                            0, 0, 1,
+                                            1, 0, 0);
 
-	tf::Matrix3x3 globalRotation_rh = tf3d;
-	tf::Vector3 globalTranslation_rh = cameraTranslation_rh * rotation270degXZ;
+    static tf::TransformBroadcaster br;
 
-	tf::Quaternion tfqt;
-	globalRotation_rh.getRotation(tfqt);
+    tf::Matrix3x3 globalRotation_rh = cameraRotation_rh * rotation270degXZ;
+    tf::Vector3 globalTranslation_rh = cameraTranslation_rh * rotation270degXZ;
+    tf::Transform transform = tf::Transform(globalRotation_rh, globalTranslation_rh);
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "camera_pose"));
+    
+    // transform into right handed camera frame
+    tf::Matrix3x3 rh_cameraPose(  - pose.at<float>(0,0),   pose.at<float>(0,1),   pose.at<float>(0,2),
+                                  - pose.at<float>(1,0),   pose.at<float>(1,1),   pose.at<float>(1,2),
+                                    pose.at<float>(2,0), - pose.at<float>(2,1), - pose.at<float>(2,2));
 
-	double aux = tfqt[0];
-	tfqt[0]=-tfqt[2];
-	tfqt[2]=tfqt[1];
-	tfqt[1]=aux;
-    globalRotation_rh.getEulerYPR(roll,yaw,pitch,1);
-    roll = radiansToDegrees(roll);
-    yaw = radiansToDegrees(yaw);
-    pitch = radiansToDegrees(pitch); 
-    ROS_INFO("Roll: %.2f\tYaw: %.2f\tPitch: %.2f\t", roll, yaw, pitch);
+    tf::Vector3 rh_cameraTranslation( pose.at<float>(0,3),pose.at<float>(1,3), - pose.at<float>(2,3) );
 
-	tf::Transform transform;
-	transform.setOrigin(globalTranslation_rh);
-	transform.setRotation(tfqt);
+    //rotate 270deg about z and 270deg about x
+    tf::Matrix3x3 rotation270degZX( 0, 0, 1,
+                                   -1, 0, 0,
+                                    0,-1, 0);
 
-	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "camera_pose"));
+    //publish right handed, x forward, y right, z down (NED)
+
+    tf::Quaternion q;
+    rh_cameraPose.getRotation(q);
+    geometry_msgs::PoseStamped p;
+    p.header.frame_id = "world";
+    p.pose.position.x = rh_cameraTranslation[0];
+    p.pose.position.y = rh_cameraTranslation[1];
+    p.pose.position.z = rh_cameraTranslation[2];
+    p.pose.orientation.x = q[0];
+    p.pose.orientation.y = q[1];
+    p.pose.orientation.z = q[2];
+    p.pose.orientation.w = q[3];
+
+    pos_pub.publish(p);
+
+    // POINT CLOUD
+    sensor_msgs::PointCloud cloud;
+    cloud.header.frame_id = "world";
+    std::vector<geometry_msgs::Point32> geo_points;
+    std::vector<ORB_SLAM2::MapPoint*> points = mpSLAM->GetTrackedMapPoints();
+    //cout << points.size() << endl;
+    for (std::vector<int>::size_type i = 0; i != points.size(); i++) {
+        if (points[i]) {
+            cv::Mat coords = points[i]->GetWorldPos();
+            geometry_msgs::Point32 pt;
+            pt.x = coords.at<float>(0);
+            pt.y = coords.at<float>(1);
+            pt.z = coords.at<float>(2);
+            geo_points.push_back(pt);
+        } else {
+        }
+    }
+    //cout << geo_points.size() << endl;
+    cloud.points = geo_points;
+    cloud_pub.publish(cloud);
 }
 
 void imageCallback(const sensor_msgs::CompressedImageConstPtr& msg)
 {
-	cv::Mat image;
+    cv::Mat image;
   try
   {
     cv::Mat image = cv::imdecode(cv::Mat(msg->data),1);//convert compressed image data to cv::Mat
@@ -193,5 +242,3 @@ void imageCallback(const sensor_msgs::CompressedImageConstPtr& msg)
     ROS_ERROR("Could not convert to image!");
   }
 }
-
-
